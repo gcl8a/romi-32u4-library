e2e68490b97e49ba10c99625bf83df00cba5d275
Greg <engineering.supergenius@gmail.com>
2020-08-18 15:30:21 -0400

    And their #includes...

--- src/Romi32U4.h
+++ src/Romi32U4.h
@@ -15,9 +15,9 @@
 
 #include <FastGPIO.h>
 #include <Romi32U4Buttons.h>
-#include <Romi32U4Buzzer.h>
+//#include <Romi32U4Buzzer.h>
 #include <Romi32U4Encoders.h>
-#include <Romi32U4LCD.h>
+//#include <Romi32U4LCD.h>
 #include <Romi32U4Motors.h>
 
 // TODO: servo support
