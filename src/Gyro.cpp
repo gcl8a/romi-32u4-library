172b862a5cf283437ca7b33cb0433f29e48d6aba
Greg <engineering.supergenius@gmail.com>
2020-09-05 20:46:17 -0400

    fixed merge conflict

--- src/Gyro.cpp
+++ src/Gyro.cpp
@@ -17,28 +17,6 @@ void Gyro::setup()
   }
   imu.enableDefault();
   imu.setFullScaleGyro(LSM6::GYRO_FS1000);
-<<<<<<< HEAD
-  imu.setGyroDataOutputRate(LSM6::GYRO_ODR52);
-
-  // Sample the z velocities
-  long sum = 0;
-  int samples = 2000;
-  for (int i = 0; i < samples; i++)
-  {
-    while ((imu.getStatus() & 0x02) == 0)
-      ;
-    imu.readGyro();
-    sum += imu.g.z;
-  }
-  average = sum / samples;
-  imu.setGyroDataOutputRate(LSM6::GYRO_ODR104);
-
-  reset();
-}
-
-void Gyro::reset()
-{
-=======
   imu.setGyroDataOutputRate(LSM6::GYRO_ODR104);
 
   reset();
@@ -56,7 +34,6 @@ void Gyro::reset()
     delay(10);
   }
   average = sum / samples;
->>>>>>> cbae11412a2f41febb40f316b929f79b547c4311
   heading = 0.0;
   gyroTimer = millis() + 10;
   lastTime = 0;
@@ -68,11 +45,7 @@ float Gyro::getHeading() { return heading; }
 
 void Gyro::loop()
 {
-<<<<<<< HEAD
   if (imu.getStatus() & 0x02)
-=======
-  if (imu.getStatus() & 0x02) 
->>>>>>> cbae11412a2f41febb40f316b929f79b547c4311
   {
     imu.readGyro();
     heading += (imu.g.z - average) * 0.035 / 104;
