e0c54ab1e30cd34e8b1c1a3474c63229fc26838e
Greg <lewin.gc@gmail.com>
2021-12-05 09:49:30 -0500

    pins

--- src/wpi-32u4-lib.h
+++ src/wpi-32u4-lib.h
@@ -3,8 +3,8 @@
 #include <Romi32U4Motors.h>
 
 // these should move to their respective files??? or move all defines here?
-#define  RIGHT_LINE_SENSE A2
-#define  MIDDLE_LINE_SENSE A3
-#define  LEFT_LINE_SENSE A4
+#define  RIGHT_LINE_SENSE A4
+//#define  MIDDLE_LINE_SENSE A3
+#define  LEFT_LINE_SENSE A3
 
 #define SERVO_PIN 6
