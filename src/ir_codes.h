296b66174b27042d158ce1e2d7f33507463a4a19
Greg <lewin.gc@gmail.com>
2021-12-11 09:24:30 -0500

    proper remote

--- src/ir_codes.h
+++ src/ir_codes.h
@@ -1,5 +1,32 @@
-#ifndef __IR_CODES_H
-#define __IR_CODES_H
+#pragma once
+
+#define VOLminus    0
+#define PLAY_PAUSE  1
+#define VOLplus     2
+
+#define SETUP_BTN   4
+#define UP_ARROW    5
+#define STOP_MODE   6
+
+#define LEFT_ARROW  8
+#define ENTER_SAVE  9
+#define RIGHT_ARROW 10
+
+#define NUM_0_10    12
+#define DOWN_ARROW  13
+#define REWIND      14
+
+#define NUM_1       16
+#define NUM_2       17
+#define NUM_3       18
+
+#define NUM_4       20
+#define NUM_5       21
+#define NUM_6       22
+
+#define NUM_7       24
+#define NUM_8       25
+#define NUM_9       26
 
 /**
  * THESE CODES DO NOT WORK FOR THE REMOTE THAT COMES WITH THE ROMI.
@@ -7,30 +34,29 @@
  * They are for an old TV remote that I use to test code.
  */
 
-#define CHplus      8
-#define CHminus     9
+// #define CHplus      8
+// #define CHminus     9
 
-#define VOLplus     6
-#define VOLminus    7
+// #define VOLplus     6
+// #define VOLminus    7
 
-#define NUM_0       16
-#define NUM_2       18
-#define NUM_4       20
-#define NUM_6       22
-#define NUM_8       24
-#define NUM_9       25
+// #define NUM_0       16
+// #define NUM_2       18
+// #define NUM_4       20
+// #define NUM_6       22
+// #define NUM_8       24
+// #define NUM_9       25
 
-#define INFO        40
+// #define INFO        40
 
-#define PREV        32
-#define BACK        29
+// #define PREV        32
+// #define BACK        29
 
-#define MENU        27
+// #define MENU        27
 
-#define OK          1
-#define UP          2
-#define DOWN        3
-#define LEFT        4
-#define RIGHT       5
+// #define OK          1
+// #define UP          2
+// #define DOWN        3
+// #define LEFT        4
+// #define RIGHT       5
 
-#endif
